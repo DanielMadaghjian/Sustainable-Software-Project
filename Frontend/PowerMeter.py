82381606c91ea186338ab5af747fd4218765f424
Darragh Clifford <dcliffor@tcd.ie>
2023-02-12 17:56:24 +0000

    GUI more consistent across OSs, test works Win/Mac

--- Frontend/PowerMeter.py
+++ Frontend/PowerMeter.py
@@ -1,6 +1,7 @@
 import random
 import time
 import tkinter as tk
+import platform as os
 root = tk.Tk()
 # root.geometry("878x535")
 root.title("Power Meter")
@@ -10,6 +11,10 @@ root.resizable(False, False)
 durationInput = 5
 wattInput = 175
 peakWattInput = 200
+
+Windows = False
+if os.platform().__contains__("Windows"):
+    Windows = True
 
 def navToSettings():
     navSettings = tk.Toplevel(root)
@@ -39,15 +44,21 @@ durationLabel = tk.Label(root, text="%d SECOND" %durationInput, font=('Arial Lig
 titleLabel = tk.Label(root, text="Power Consumption", font=('Arial Bold', 32), bg="white", fg="black")
 durationLabel.grid(row = 0, column = 0, columnspan = 2, rowspan = 1, sticky = tk.SW, padx = 40, pady = 0)
 titleLabel.grid(row = 1, column = 0, columnspan = 2, rowspan = 1, sticky = tk.NW, padx = 40, pady = 0)
-carbonButton = tk.Button(root, text="Start", height=6, width=10, command=start5SecondTest)
-graphButton = tk.Button(root, text="View Graph", height=6, width=10)
+buttonWidth = 10
+if Windows: 
+    buttonWidth = 15
+carbonButton = tk.Button(root, text="Start", height=6, width=buttonWidth, command=start5SecondTest)
+graphButton = tk.Button(root, text="View Graph", height=6, width=buttonWidth)
 carbonButton.grid(row = 2, column = 0, sticky = tk.E, padx = 40, pady = 2)
 graphButton.grid(row = 2, column = 1, sticky = tk.W, padx = 0, pady = 2)
 canvas = tk.Canvas(root, background="white", height=400, width=400, highlightthickness=0)
 intCircle = canvas.create_oval(15, 15, 385, 385, outline="white", fill="#EEEEEE")
 backArc = canvas.create_arc(5, 5, 395, 395, outline="black", style=tk.ARC, width=6, start=315, extent="270")
 # wattText = canvas.create_text(200, 210, text='Click "Start" to run\na 5-second test', font=('Arial Bold', 28), fill="black", justify="center")
-startText = canvas.create_text(200, 200, text='Press "Start"', font=('Arial Bold', 48), fill="black", justify="center")
+startFontSize = 48
+if Windows:
+    startFontSize = 40
+startText = canvas.create_text(200, 200, text='Press "Start"', font=('Arial Bold', startFontSize), fill="black", justify="center")
 subtitleText = canvas.create_text(200, 245, text="TO START A 5-SECOND TEST", font=('Arial Light', 18), fill="gray", justify="center")
 canvas.grid(row = 0, column = 2, columnspan = 2, rowspan = 3, padx = 40, pady = 40)
 settingsButton = tk.Button(root, text = "Settings", height = 2, width = 10, bg="white", fg="black", command=navToSettings)
--- Frontend/Test.py
+++ Frontend/Test.py
@@ -1,10 +1,21 @@
-# NB -  Ensure no GUI are open,
+# NB -  Install pyautogui by running:
+#       macOS/Linux: 'python3 -m pip install pyautogui'
+#       Windows:     'py -m pip install pyautogui'
+#       If you are using macOS, install Quartz with 'python3 -m pip install pyobjc-framework-Quartz'
+#       Ensure no GUI are open,
 #       Run PowerMeter.py,
 #       then run this file in debug mode
 
 import time
 import pyautogui as gui
-from Quartz import CGWindowListCopyWindowInfo, kCGNullWindowID, kCGWindowListOptionAll
+macOS = False
+try:
+    from Quartz import CGWindowListCopyWindowInfo, kCGNullWindowID, kCGWindowListOptionAll
+    print("Using macOS, importing Quartz...")
+    macOS = True
+except:
+    print("Using Windows, using pygetwindow instead of Quartz...")
+    import pygetwindow as pgw
 
 screenWidth, screenHeight = gui.size()
 
@@ -17,7 +28,7 @@ screenWidth, screenHeight = gui.size()
 
 def testStartButton():
     #Bug - does not show mouse clicked but shows number change
-    gui.moveTo(windowX + 135, windowY + windowHeader + 380)
+    gui.moveTo((windowX + 140) * offset, (windowY + 380) * offset + windowHeader)
     gui.sleep(0.5)
     gui.click()
     gui.sleep(0.1)
@@ -27,33 +38,52 @@ def testStartButton():
 
 def testSettings():
     gui.sleep(1)
-    gui.moveTo(windowX + windowWidth - 75, windowY + windowHeight - 30)
+    gui.moveTo((windowX + windowWidth) - 75 * offset, (windowY + windowHeight) - 30 * offset)
     gui.click()
 
-allWindows = CGWindowListCopyWindowInfo(kCGWindowListOptionAll, kCGNullWindowID)
-windowOnScreen = False
-for window in allWindows:
-    try:
-        if window['kCGWindowOwnerName'] == "Python" and window['kCGWindowIsOnscreen'] == 1:
-            print(window)
-            windowOnScreen = True
-            windowBounds = window['kCGWindowBounds']
-    except:
-        pass
-    if windowOnScreen == True:
-        break
-# Code using CGWindowListCopyWindowInfo inspired by Suyash Patel (https://stackoverflow.com/users/3706058/suyash-patel)
-if windowOnScreen == False:
-    print('Unable to find window, please run "PowerMeter.py"')
-    quit()
+if macOS == True:
+    allWindows = CGWindowListCopyWindowInfo(kCGWindowListOptionAll, kCGNullWindowID)
+    windowOnScreen = False
+    for window in allWindows:
+        try:
+            if window['kCGWindowOwnerName'] == "Python" and window['kCGWindowIsOnscreen'] == 1:
+                print(window)
+                windowOnScreen = True
+                windowBounds = window['kCGWindowBounds']
+        except:
+            pass
+        if windowOnScreen == True:
+            break
+    # Code using CGWindowListCopyWindowInfo inspired by Suyash Patel (https://stackoverflow.com/users/3706058/suyash-patel)
+    if windowOnScreen == False:
+        print('Unable to find window, please run "PowerMeter.py"')
+        quit()
 
-print("Window found! Running tests...")
-windowHeight = windowBounds['Height']
-windowWidth = windowBounds['Width']
-windowX = windowBounds['X']
-windowY = windowBounds['Y']
-print(windowHeight, windowWidth, windowX, windowY)
-windowHeader = 25
+    print("Window found! Running tests...")
+    windowHeight = windowBounds['Height']
+    windowWidth = windowBounds['Width']
+    windowX = windowBounds['X']
+    windowY = windowBounds['Y']
+    windowHeader = 25
+    offset = 1 
+    # The offset is in place as the pixel measurements are smaller on macOS
+else:
+    try:
+        window = pgw.getWindowsWithTitle('Power Meter')[0]
+        window.moveTo(30,30)
+        print("Window found! Running tests...")
+        
+    except:
+        print('Unable to find window, please run "PowerMeter.py"')
+        quit()
+    
+    windowHeader = 35
+    windowHeight = window.height
+    windowWidth = window.width
+    windowX = 30
+    windowY = 30
+    offset = 1.5
+    # The offset is in place as the pixel measurements are bigger on Windows
 
 # testOpenGUI()
 testStartButton()
