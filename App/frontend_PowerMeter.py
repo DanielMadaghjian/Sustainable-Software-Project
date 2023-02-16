08bb6e3ae5ea1218c0cbd8f413bf2a3cb08c9c41
Darragh Clifford <dcliffor@tcd.ie>
2023-02-16 10:37:17 +0000

    Function call changed, still calls func at launch

--- App/frontend_PowerMeter.py
+++ App/frontend_PowerMeter.py
@@ -26,21 +26,7 @@ def navToSettings():
     settingsLabel.grid(row = 1, column = 0, columnspan = 2, rowspan = 1, sticky = tk.NW, padx = 40, pady = 0)
     tk.Label(navSettings, text ="Settings", font=('Arial Bold', 32), bg="white", fg="black").pack()
 
-def start5SecondTest():
-    durationInput = 5
-    ##Calling the analysis function
-    wattInput = backend_analysis.dataAnalysis(durationInput)
-    peakWattInput = backend_analysis.dataAnalysis(durationInput)
-    ##
-    canvas.create_oval(15, 15, 385, 385, outline="white", fill="#EEEEEE")
-    canvas.create_text(200, 200, text="%d W" %wattInput, font=('Arial Bold', 64), fill="black", justify="center")
-    canvas.create_text(200, 245, text="PEAK %d W" %peakWattInput, font=('Arial Light', 18), fill="gray", justify="center")
-    # backArc
-    canvas.create_arc(5, 5, 395, 395, outline="black", style=tk.ARC, width=6, start=315, extent="270")
-    # The arc starts from the right and is a total of 270°, so 270-((avg/peak) * 270) in grey will give the percentage visually
-    # progressArc
-    canvas.create_arc(5, 5, 395, 395, outline="#EEEEEE", style=tk.ARC, width=8, start=315, extent="%d" % round(270-((wattInput/peakWattInput)*270)))
-    print(round(270-((wattInput/peakWattInput)*270)))
+
 
 durationLabel = tk.Label(root, text="%d SECOND" %durationInput, font=('Arial Light', 18), bg="white", fg="gray")
 titleLabel = tk.Label(root, text="Power Consumption", font=('Arial Bold', 32), bg="white", fg="black")
@@ -49,7 +35,8 @@ titleLabel.grid(row = 1, column = 0, columnspan = 2, rowspan = 1, sticky = tk.NW
 buttonWidth = 10
 if Windows: 
     buttonWidth = 15
-carbonButton = tk.Button(root, text="Start", height=6, width=buttonWidth, command=start5SecondTest)
+carbonButton = tk.Button(root, text="Start", height=6, width=buttonWidth)
+carbonButton['command'] = lambda: startTest(5)
 graphButton = tk.Button(root, text="View Graph", height=6, width=buttonWidth)
 carbonButton.grid(row = 2, column = 0, sticky = tk.E, padx = 40, pady = 2)
 graphButton.grid(row = 2, column = 1, sticky = tk.W, padx = 0, pady = 2)
@@ -57,14 +44,29 @@ canvas = tk.Canvas(root, background="white", height=400, width=400, highlightthi
 intCircle = canvas.create_oval(15, 15, 385, 385, outline="white", fill="#EEEEEE")
 backArc = canvas.create_arc(5, 5, 395, 395, outline="black", style=tk.ARC, width=6, start=315, extent="270")
 # wattText = canvas.create_text(200, 210, text='Click "Start" to run\na 5-second test', font=('Arial Bold', 28), fill="black", justify="center")
-startFontSize = 48
+boldFontSize = 48
 if Windows:
-    startFontSize = 40
-startText = canvas.create_text(200, 200, text='Press "Start"', font=('Arial Bold', startFontSize), fill="black", justify="center")
+    boldFontSize = 40
+startText = canvas.create_text(200, 200, text='Press "Start"', font=('Arial Bold', boldFontSize), fill="black", justify="center")
 subtitleText = canvas.create_text(200, 245, text="TO START A 5-SECOND TEST", font=('Arial Light', 18), fill="gray", justify="center")
 canvas.grid(row = 0, column = 2, columnspan = 2, rowspan = 3, padx = 40, pady = 40)
 settingsButton = tk.Button(root, text = "Settings", height = 2, width = 10, bg="white", fg="black", command=navToSettings)
 settingsButton.grid(row = 3, column = 3, padx = 10, pady = 10, sticky = tk.SE)
+
+def startTest(durationInput):
+    ##Calling the analysis function
+    wattInput = backend_analysis.dataAnalysis(durationInput)[0]
+    peakWattInput = wattInput
+    ##
+    canvas.create_oval(15, 15, 385, 385, outline="white", fill="#EEEEEE")
+    canvas.create_text(200, 200, text=str(wattInput) + " W", font=('Arial Bold', boldFontSize+16), fill="black", justify="center")
+    canvas.create_text(200, 245, text="PEAK " + str(peakWattInput) + " W", font=('Arial Light', 18), fill="gray", justify="center")
+    # backArc
+    canvas.create_arc(5, 5, 395, 395, outline="black", style=tk.ARC, width=6, start=315, extent="270")
+    # The arc starts from the right and is a total of 270°, so 270-((avg/peak) * 270) in grey will give the percentage visually
+    # progressArc
+    canvas.create_arc(5, 5, 395, 395, outline="#EEEEEE", style=tk.ARC, width=8, start=315, extent="%d" % round(270-((wattInput/peakWattInput)*270)))
+    print(round(270-((wattInput/peakWattInput)*270)))
 
 root.mainloop()
 
