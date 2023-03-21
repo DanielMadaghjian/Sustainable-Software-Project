d3e73f81e276eca9f3d64b3cb352da0fcf6866d4
Darragh Condron <condrond@tcd.ie>
2023-03-21 16:17:20 +0000

    Screen changes if variable manually changed

--- App/frontend_PowerMeter.py
+++ App/frontend_PowerMeter.py
@@ -7,6 +7,7 @@ from random import randint
 import matplotlib.pyplot as plt
 import platform as os
 import backend_analysis
+
 root = tk.Tk()
 # root.geometry("878x535")
 root.title("Power Meter")
@@ -14,9 +15,8 @@ backgroundColour = '#DAEFD2'
 root.configure(background=backgroundColour)
 # root.configure(background='white')
 root.resizable(False, False)
-screen = 0
-running = True
 
+screen = 0
 durationInput = 5
 wattInput = 175
 peakWattInput = 200
@@ -90,90 +90,96 @@ def graphToDisplay():
 #     canvas.create_arc(5, 5, 395, 395, outline="black", style=tk.ARC, width=6, start=315, extent="270")
 
 def changeScreen(newScreen):
+    global screen
+    print (screen)
     screen = newScreen
+    print(newScreen)
+    print (screen)
 
-while running:
-    if (screen == 0):
-        startImage = tk.PhotoImage(file='App/Test.png')
-        startImage = startImage.subsample(2)
-        durationLabel = tk.Label(root, text="%d SECOND" %durationInput, font=('Arial Light', 18), bg=backgroundColour, fg="black")
-        carbonButton = tk.Button(root, text="Start", image=startImage, height=150, width=150, borderwidth=0)
-        carbonButton['command'] = lambda: changeScreen(1)
-        carbonButton.grid(row = 2, column = 0, sticky = tk.E, padx = 40, pady = 2)
+screen = 1
+if (screen == 0):
+    startImage = tk.PhotoImage(file='App/Test.png')
+    startImage = startImage.subsample(2)
+    carbonButton = tk.Button(root, text="Start", image=startImage, height=150, width=150, borderwidth=0,)
+    carbonButton['command'] = lambda: changeScreen(1)
+    carbonButton.grid(row = 2, column = 0, sticky = tk.E, padx = 40, pady = 2)
 
-    elif (screen == 1):
-        durationLabel = tk.Label(root, text="%d SECOND" %durationInput, font=('Arial Light', 18), bg=backgroundColour, fg="black")
-        titleLabel = tk.Label(root, text="Power Consumption", font=('Arial Bold', 32), bg=backgroundColour, fg="black")
-        durationLabel.grid(row = 0, column = 0, columnspan = 2, rowspan = 1, sticky = tk.SW, padx = 40, pady = 0)
-        titleLabel.grid(row = 1, column = 0, columnspan = 2, rowspan = 1, sticky = tk.NW, padx = 40, pady = 0)
-        buttonWidth = 10
-        if Windows: 
-            buttonWidth = 15
+if (screen == 1):
+    homeButton = tk.Button(root, text="Home", height=25, width=25)
+    homeButton['command'] = lambda: changeScreen(0)
+    homeButton.grid(row = 0, column = 0, sticky = tk.E, padx = 40, pady = 2)
+    durationLabel = tk.Label(root, text="%d SECOND" %durationInput, font=('Arial Light', 18), bg=backgroundColour, fg="black")
+    titleLabel = tk.Label(root, text="Power Consumption", font=('Arial Bold', 32), bg=backgroundColour, fg="black")
+    durationLabel.grid(row = 0, column = 0, columnspan = 2, rowspan = 1, sticky = tk.SW, padx = 40, pady = 0)
+    titleLabel.grid(row = 1, column = 0, columnspan = 2, rowspan = 1, sticky = tk.NW, padx = 40, pady = 0)
+    buttonWidth = 10
+    if Windows: 
+        buttonWidth = 15
 
-        startImage = tk.PhotoImage(file='App/Test.png')
-        startImage = startImage.subsample(2)
+    startImage = tk.PhotoImage(file='App/Test.png')
+    startImage = startImage.subsample(2)
 
-        carbonButton = tk.Button(root, text="Start", image=startImage, height=150, width=150, borderwidth=0)
-        carbonButton['command'] = lambda: startTest(5)
+    carbonButton = tk.Button(root, text="Start", image=startImage, height=150, width=150, borderwidth=0)
+    carbonButton['command'] = lambda: startTest(5)
 
-        graphImage = tk.PhotoImage(file='App/Graph.png')
-        graphImage = graphImage.subsample(2)
+    graphImage = tk.PhotoImage(file='App/Graph.png')
+    graphImage = graphImage.subsample(2)
 
-        carbonButton.grid(row = 2, column = 0, sticky = tk.E, padx = 40, pady = 2)
-        # graphButton = tk.Button(root, text="View Graph", image=graphImage, height=150, width=150, borderwidth=0,command=graphToDisplay)
-        # graphButton.grid(row = 2, column = 1, sticky = tk.W, padx = 0, pady = 2)
-        canvas = tk.Canvas(root, background=backgroundColour, height=400, width=400, highlightthickness=0)
-        intCircle = canvas.create_oval(15, 15, 385, 385, outline="white", fill="white")
-        backArc = canvas.create_arc(5, 5, 395, 395, outline="black", style=tk.ARC, width=6, start=315, extent="270")
-        # wattText = canvas.create_text(200, 210, text='Click "Start" to run\na 5-second test', font=('Arial Bold', 28), fill="black", justify="center")
-        boldFontSize = 48
-        if Windows:
-            boldFontSize = 40
-        startText = canvas.create_text(200, 200, text='Press "Start"', font=('Arial Bold', boldFontSize), fill="black", justify="center")
-        subtitleText = canvas.create_text(200, 245, text="TO START A 5-SECOND TEST", font=('Arial Light', 18), fill="gray", justify="center")
-        canvas.grid(row = 0, column = 2, columnspan = 2, rowspan = 3, padx = 40, pady = 40)
+    carbonButton.grid(row = 2, column = 0, sticky = tk.E, padx = 40, pady = 2)
+    # graphButton = tk.Button(root, text="View Graph", image=graphImage, height=150, width=150, borderwidth=0,command=graphToDisplay)
+    # graphButton.grid(row = 2, column = 1, sticky = tk.W, padx = 0, pady = 2)
+    canvas = tk.Canvas(root, background=backgroundColour, height=400, width=400, highlightthickness=0)
+    intCircle = canvas.create_oval(15, 15, 385, 385, outline="white", fill="white")
+    backArc = canvas.create_arc(5, 5, 395, 395, outline="black", style=tk.ARC, width=6, start=315, extent="270")
+    # wattText = canvas.create_text(200, 210, text='Click "Start" to run\na 5-second test', font=('Arial Bold', 28), fill="black", justify="center")
+    boldFontSize = 48
+    if Windows:
+        boldFontSize = 40
+    startText = canvas.create_text(200, 200, text='Press "Start"', font=('Arial Bold', boldFontSize), fill="black", justify="center")
+    subtitleText = canvas.create_text(200, 245, text="TO START A 5-SECOND TEST", font=('Arial Light', 18), fill="gray", justify="center")
+    canvas.grid(row = 0, column = 2, columnspan = 2, rowspan = 3, padx = 40, pady = 40)
 
-        values = tk.Canvas(root, background=backgroundColour,height=150, width=150, highlightthickness=0)
-        hardware = values.create_rectangle(5, 5, 150, 250, outline=backgroundColour,fill=backgroundColour)
-        GPU = values.create_text(40,40,text = "GPU : ",font =('Arial Bold', 18),fill="black", justify="center")
-        CPU = values.create_text(40,80,text = "CPU :",font =('Arial Bold', 18),fill="black", justify="center")
-        GPU = values.create_text(40,120,text = "RAM :",font =('Arial Bold', 18),fill="black", justify="center")
-        GPU_values = values.create_text(100,40,text = "TBC",font =('Arial Light', 12),fill="black", justify="center")
-        CPU_values = values.create_text(100,80,text = "TBC",font =('Arial Light', 12),fill="black", justify="center")
-        GPU_values = values.create_text(100,120,text = "TBC",font =('Arial Light', 12),fill="black", justify="center")
-        values.grid(row = 2, column = 1, sticky = tk.W, padx = 0, pady = 2)
+    values = tk.Canvas(root, background=backgroundColour,height=150, width=150, highlightthickness=0)
+    hardware = values.create_rectangle(5, 5, 150, 250, outline=backgroundColour,fill=backgroundColour)
+    GPU = values.create_text(40,40,text = "GPU : ",font =('Arial Bold', 18),fill="black", justify="center")
+    CPU = values.create_text(40,80,text = "CPU :",font =('Arial Bold', 18),fill="black", justify="center")
+    GPU = values.create_text(40,120,text = "RAM :",font =('Arial Bold', 18),fill="black", justify="center")
+    GPU_values = values.create_text(100,40,text = "TBC",font =('Arial Light', 12),fill="black", justify="center")
+    CPU_values = values.create_text(100,80,text = "TBC",font =('Arial Light', 12),fill="black", justify="center")
+    GPU_values = values.create_text(100,120,text = "TBC",font =('Arial Light', 12),fill="black", justify="center")
+    values.grid(row = 2, column = 1, sticky = tk.W, padx = 0, pady = 2)
 
-        settingsImage = tk.PhotoImage(file='App/Settings.png')
-        settingsImage = settingsImage.subsample(3)
+    settingsImage = tk.PhotoImage(file='App/Settings.png')
+    settingsImage = settingsImage.subsample(3)
 
-        settingsButton = tk.Button(root, text="Settings", image=settingsImage, height=50, width=100, borderwidth=0, command=lambda: navToSettings())
-        settingsButton.grid(row = 3, column = 3, padx = 10, pady = 10, sticky = tk.SE)
+    settingsButton = tk.Button(root, text="Settings", image=settingsImage, height=50, width=100, borderwidth=0, command=lambda: navToSettings())
+    settingsButton.grid(row = 3, column = 3, padx = 10, pady = 10, sticky = tk.SE)
             
-        def startTest(durationInput):
-            ##Calling the analysis function
-            backendData = backend_analysis.dataAnalysis(durationInput, countryID[intCountry])
-            wattInput = backendData[0]
-            carbonEmissions = (((backendData[1])/60)/12)*1000000
-            ##
-            canvas.create_oval(15, 15, 385, 385, outline="white", fill="white")
-            canvas.create_text(200, 200, text=str(round(wattInput, 2)) + " W", font=('Arial Bold', boldFontSize+16), fill="black", justify="center")
-            canvas.create_text(200, 245, text=str(round(carbonEmissions, 2)) + " mgCO₂", font=('Arial Light', 18), fill="gray", justify="center")
-            canvas.create_text(200, 160, text="Ø", font=('Arial Light', 18), fill="gray", justify="center")
-            # backArc
-            canvas.create_arc(5, 5, 395, 395, outline="black", style=tk.ARC, width=6, start=315, extent="270")
-            # The arc starts from the right and is a total of 270°, so 270-((avg/peak) * 270) in grey will give the percentage visually
-            # progressArc
-            canvas.create_arc(5, 5, 395, 395, outline="white", style=tk.ARC, width=8, start=315, extent="%d" % round(270-((wattInput/peakWattInput)*270)))
-            print(round(270-((wattInput/peakWattInput)*270)))
-            values.create_rectangle(5, 5, 150, 250, outline=backgroundColour,fill=backgroundColour)
-            values.create_text(40,40,text = "GPU : ",font =('Arial Bold', 18),fill="black", justify="center")
-            values.create_text(40,80,text = "CPU :",font =('Arial Bold', 18),fill="black", justify="center")
-            values.create_text(40,120,text = "RAM :",font =('Arial Bold', 18),fill="black", justify="center")
-            if backendData[2] == 0:
-                values.create_text(100,40,text = "N/A",font =('Arial Light', 12),fill="black", justify="center")
-            else:
-                values.create_text(100,40,text = str(round(backendData[2],2)) + " W",font =('Arial Light', 12),fill="black", justify="center")
-            values.create_text(100,80,text = str(round(backendData[3],2)) + " W",font =('Arial Light', 12),fill="black", justify="center")
-            values.create_text(100,120,text = str(round(backendData[4],2))+ " W",font =('Arial Light', 12),fill="black", justify="center")
+    def startTest(durationInput):
+        ##Calling the analysis function
+        backendData = backend_analysis.dataAnalysis(durationInput, countryID[intCountry])
+        wattInput = backendData[0]
+        carbonEmissions = (((backendData[1])/60)/12)*1000000
+        ##
+        canvas.create_oval(15, 15, 385, 385, outline="white", fill="white")
+        canvas.create_text(200, 200, text=str(round(wattInput, 2)) + " W", font=('Arial Bold', boldFontSize+16), fill="black", justify="center")
+        canvas.create_text(200, 245, text=str(round(carbonEmissions, 2)) + " mgCO₂", font=('Arial Light', 18), fill="gray", justify="center")
+        canvas.create_text(200, 160, text="Ø", font=('Arial Light', 18), fill="gray", justify="center")
+        # backArc
+        canvas.create_arc(5, 5, 395, 395, outline="black", style=tk.ARC, width=6, start=315, extent="270")
+        # The arc starts from the right and is a total of 270°, so 270-((avg/peak) * 270) in grey will give the percentage visually
+        # progressArc
+        canvas.create_arc(5, 5, 395, 395, outline="white", style=tk.ARC, width=8, start=315, extent="%d" % round(270-((wattInput/peakWattInput)*270)))
+        print(round(270-((wattInput/peakWattInput)*270)))
+        values.create_rectangle(5, 5, 150, 250, outline=backgroundColour,fill=backgroundColour)
+        values.create_text(40,40,text = "GPU : ",font =('Arial Bold', 18),fill="black", justify="center")
+        values.create_text(40,80,text = "CPU :",font =('Arial Bold', 18),fill="black", justify="center")
+        values.create_text(40,120,text = "RAM :",font =('Arial Bold', 18),fill="black", justify="center")
+        if backendData[2] == 0:
+            values.create_text(100,40,text = "N/A",font =('Arial Light', 12),fill="black", justify="center")
+        else:
+            values.create_text(100,40,text = str(round(backendData[2],2)) + " W",font =('Arial Light', 12),fill="black", justify="center")
+        values.create_text(100,80,text = str(round(backendData[3],2)) + " W",font =('Arial Light', 12),fill="black", justify="center")
+        values.create_text(100,120,text = str(round(backendData[4],2))+ " W",font =('Arial Light', 12),fill="black", justify="center")
 
 root.mainloop()
