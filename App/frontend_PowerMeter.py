1ebb1129168dca7baadde31ec184d5e5ea4136d5
dctcd <dcliffor@tcd.ie>
2023-03-24 12:39:03 +0000

    Added screens for testing app incl progress screen

--- App/frontend_PowerMeter.py
+++ App/frontend_PowerMeter.py
@@ -11,6 +11,17 @@ backgroundColour = '#DAEFD2'
 
 previousScreen = tk.Frame
 baselineRun = False
+countdownDisplay = -1
+
+globalCanvas = tk.Canvas
+
+def runAppTest(controller) :
+    controller.show_frame(AppTesting)
+    # for sec in range(0, 60, 1):
+    #     timeRemaining = str(sec).zfill(2)
+    #     globalCanvas.create_oval(15, 15, 385, 385, outline="white", fill="white")
+    #     globalCanvas.create_text(200, 200, text=timeRemaining, font=('Arial Bold', 40), fill="black", justify="center")
+    #     globalCanvas.update()
 
 class tkinterApp(tk.Tk):
     def __init__(self, *args, **kwargs):
@@ -22,7 +33,7 @@ class tkinterApp(tk.Tk):
         container.grid_columnconfigure(0, weight = 1)
         self.frames = {} 
 
-        for F in (StartPage, Page1, AppTestHome, BaselineTest, AppTesting, AppResults, SettingsPage):
+        for F in (StartPage, AppTestHome, TimedTest, BaselineTest, AppTesting, AppResults, SettingsPage):
             frame = F(container, self)
             self.frames[F] = frame
             frame.grid(row = 0, column = 0, sticky ="nsew")
@@ -77,14 +88,14 @@ class tkinterApp(tk.Tk):
         previousScreen = StartPage
         controller.show_frame(SettingsPage)
 
-    def settingsPage1(self,controller):
-        global previousScreen
-        previousScreen = Page1
-        controller.show_frame(SettingsPage)
-
     def settingsAppTestHome(self,controller):
         global previousScreen
         previousScreen = AppTestHome
+        controller.show_frame(SettingsPage)
+
+    def settingsTimedTest(self,controller):
+        global previousScreen
+        previousScreen = TimedTest
         controller.show_frame(SettingsPage)
     
 class StartPage(tk.Frame):
@@ -92,8 +103,8 @@ class StartPage(tk.Frame):
         backgroundColour = '#DAEFD2'
         tk.Frame.__init__(self, parent, background=backgroundColour)
 
-        button1 = tk.Button(self, text ="Continuous Usage",command = lambda : controller.show_frame(Page1))
-        button2 = tk.Button(self, text ="Individual Usage",command = lambda : controller.show_frame(AppTestHome))
+        button1 = tk.Button(self, text ="Test an App's Power Usage",command = lambda : controller.show_frame(AppTestHome))
+        button2 = tk.Button(self, text ="Test for a Specified Time",command = lambda : controller.show_frame(TimedTest))
 
         settingsImage = tk.PhotoImage(file='App/Settings.png')
         settingsImage = settingsImage.subsample(3)
@@ -105,9 +116,8 @@ class StartPage(tk.Frame):
         button1.grid(row = 1, column = 1,padx = 10, pady = 10)
         button2.grid(row = 2, column = 1, padx = 10, pady = 10)
         settingsButton.grid(row = 3, column = 3, padx = 10, pady = 10, sticky = tk.SE)
-  
 
-class Page1(tk.Frame):
+class AppTestHome(tk.Frame):
      
     def __init__(self, parent, controller):
         # backgroundColour = '#DAEFD2' 
@@ -116,7 +126,7 @@ class Page1(tk.Frame):
         settingsImage = tk.PhotoImage(file='App/Settings.png')
         settingsImage = settingsImage.subsample(3)
         settingsButton = tk.Button(self, text="Settings", image=settingsImage, height = 50, width = 100, borderwidth = 0, 
-                                   command = lambda : controller.settingsPage1(controller))
+                                   command = lambda : controller.settingsAppTestHome(controller))
         #command = lambda : controller.navToSettings())
         settingsButton.image = settingsImage
 
@@ -127,13 +137,13 @@ class Page1(tk.Frame):
         baselineImage = tk.PhotoImage(file='App/Baseline.png')
         baselineImage = baselineImage.subsample(2)
         baselineButton = tk.Button(self,text="Start", image = baselineImage, height = 150, width = 150, borderwidth = 0, 
-                                 command = lambda : controller.navTo)
+                                 command = lambda : controller.show_frame(BaselineTest))
         baselineButton.image = baselineImage
 
         appTestImage = tk.PhotoImage(file='App/AppTest.png')
         appTestImage = appTestImage.subsample(2)
         appTestButton = tk.Button(self,text="Start", image = appTestImage, height = 150, width = 150, borderwidth = 0, 
-                                 command = lambda : controller.startTest(5,canvas,values))
+                                 command = lambda : runAppTest(controller))
         appTestButton.image = appTestImage
 
         backImage = tk.PhotoImage(file='App/back.png')
@@ -143,7 +153,6 @@ class Page1(tk.Frame):
         backButton.image = backImage
 
         tk.Tk.configure(self, bg='#DAEFD2')
-        returnButton = tk.Button(self, text ="Return",command = lambda : controller.show_frame(StartPage))
 
         canvas = tk.Canvas(self, background=backgroundColour, height=400, width=400, highlightthickness=0)
         canvas.create_oval(15, 15, 385, 385, outline="white", fill="white")
@@ -163,7 +172,7 @@ class Page1(tk.Frame):
         backButton.grid(row = 0, column = 0,sticky=tk.NW, padx = 5, pady = 5)
         canvas.grid(row = 0, column = 2, columnspan = 2, rowspan = 3, padx = 40, pady = 40)
   
-class AppTestHome(tk.Frame):
+class TimedTest(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent, background=backgroundColour)
 
@@ -183,7 +192,7 @@ class AppTestHome(tk.Frame):
         settingsImage = tk.PhotoImage(file='App/Settings.png')
         settingsImage = settingsImage.subsample(3)
         settingsButton = tk.Button(self, text="Settings", image=settingsImage, height = 50, width = 100, borderwidth = 0, 
-                                   command = lambda : controller.settingsAppTestHome(controller))
+                                   command = lambda : controller.settingsTimedTest(controller))
         #command = lambda : controller.navToSettings())
         settingsButton.image = settingsImage
 
@@ -212,25 +221,70 @@ class AppTestHome(tk.Frame):
         canvas.grid(row = 0, column = 2, columnspan = 2, rowspan = 3, padx = 40, pady = 40)
         values.grid(row = 2, column = 1, sticky = tk.W, padx = 0, pady = 2)
 
+        globalCanvas = canvas
+
 class BaselineTest(tk.Frame):
     def __init__(self, parent, controller):
         
         tk.Frame.__init__(self, parent, background=backgroundColour)
+        tk.Tk.configure(self, bg='#DAEFD2')
+
         
 
 class AppTesting(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent, background=backgroundColour)
+        tk.Tk.configure(self, bg='#DAEFD2')
+
+        settingsImage = tk.PhotoImage(file='App/Settings.png')
+        settingsImage = settingsImage.subsample(3)
+        settingsButton = tk.Button(self, text="Settings", image=settingsImage, height = 50, width = 100, borderwidth = 0, 
+                                   command = lambda : controller.settingsAppTestHome(controller))
+        #command = lambda : controller.navToSettings())
+        settingsButton.image = settingsImage
+
+        settingsButton.grid(row = 3, column = 3, padx = 10, pady = 10, sticky = tk.SE)
+        titleLabel = ttk.Label(self, text="Measuring...",style= 'Test.TLabel',font=('Arial Bold', 32), padding=(0,22,0,0))
+        infoLabel = ttk.Label(self, text="Please wait while we measure the idle power use of\nyour device. Please ensure that you keep all other\napps closed until this test completes.", font=('Arial Light', 15), style= 'Test.TLabel')
+
+        stopImage = tk.PhotoImage(file='App/Stop.png')
+        stopImage = stopImage.subsample(2)
+        stopButton = tk.Button(self,text="Start", image = stopImage, height = 150, width = 150, borderwidth = 0, 
+                                 command = lambda : controller.show_frame(BaselineTest))
+        stopButton.image = stopImage
+
+       
+
+        tk.Tk.configure(self, bg='#DAEFD2')
+
+        canvas = tk.Canvas(self, background=backgroundColour, height=400, width=400, highlightthickness=0)
+        canvas.create_arc(5, 5, 395, 395, outline="white", style=tk.ARC, width=6, start=315, extent="270")
+        
+        canvas.create_oval(15, 15, 385, 385, outline="white", fill="white")
+
+        carbonImage = tk.PhotoImage(file='App/Carbon.png')
+        carbonImage = carbonImage.subsample(6)
+        canvas.image = carbonImage
+        canvas.create_image(200,240,anchor=tk.S,image=carbonImage)
+        
+
+        infoLabel.grid(row = 1, column = 0, columnspan = 2, rowspan = 1, sticky = tk.NW, padx = (40,0), pady = 0)
+        titleLabel.grid(row = 0, column = 0, columnspan = 2, rowspan = 1, sticky = tk.SW, padx = 40, pady = 0)
+        stopButton.grid(row = 2, column = 0, sticky = tk.E, padx = (40,20), pady = 2)
+        settingsButton.grid(row = 3, column = 3, padx = 10, pady = 10, sticky = tk.SE)
+        canvas.grid(row = 0, column = 2, columnspan = 2, rowspan = 3, padx = 40, pady = 40)
         
 
 class AppResults(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent, background=backgroundColour)
+        tk.Tk.configure(self, bg='#DAEFD2')
         
 
 class SettingsPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent, background=backgroundColour)
+        tk.Tk.configure(self, bg='#DAEFD2')
         returnButton = tk.Button(self, text ="Return",command = lambda : controller.show_frame(previousScreen))
 
         irlButton = tk.Button(self, text ="Ireland",command = lambda : controller.show_frame(previousScreen))
